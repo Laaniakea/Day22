72601efef896d84a17eaabbe918eada5cbfd478a
unknown <edina.kurtic90@hotmail.com>
2021-03-30 10:47:28 +0200

    added ex06 Day22

---
+++ ex06/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import React, { Component } from 'react';
+import Plant from './Plant';
+class App extends Component {
+  render(){
+  return (
+    <div className="App">
+      <Plant/>
+    </div>
+  );
+}
+}
+
+export default App;
