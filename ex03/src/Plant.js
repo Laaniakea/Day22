2e4d83c3a7b507982745f99b631b8580b0cdd6fb
unknown <edina.kurtic90@hotmail.com>
2021-03-30 10:45:18 +0200

    added ex03 Day22

--- ex03/src/Plant.js
+++ ex03/src/Plant.js
@@ -1,10 +1,10 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
 
-class Plant extends Component{
-render(){
-    return<h2>This is a Plant Component!</h2>
+class Plant extends Component {
+    render() {
+        return <h2>This is a Plant Component!</h2>
 
-}
+    }
 }
 export default Plant;
